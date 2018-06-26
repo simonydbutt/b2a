7a1fcee4e2c3e77a6474c3a687c4608801ddea28
simonydbutt <simonydbutt@gmail.com>
2018-06-26 23:35:02 +0100

    BitmexDS comp

--- Backtest/main/Utils/TimeUtil.py
+++ Backtest/main/Utils/TimeUtil.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 class TimeUtil:
 
-    def getTS(self, date, timeFormat="%Y-%m-%dT%H:%M:%S.000Z"):
+    def getTS(self, date, timeFormat='%Y-%m-%dT%H:%M:%S.000Z'):
         return time.mktime(datetime.strptime(date, timeFormat).timetuple())
