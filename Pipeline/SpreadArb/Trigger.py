79fe2a175dd16ce7ca9665e177cee0fc8da74609
simonydbutt <simonydbutt@gmail.com>
2018-06-30 03:34:36 +0100

    Removed Pipeline

--- Pipeline/SpreadArb/Trigger.py
+++
@@ -1,8 +0,0 @@
-from Pipeline.SpreadArb.main.SpreadArbRun import SpreadArbRun
-
-print('\n |   ___ \  |')
-print(' __ \   ) | __ \ ')
-print(' |   | __/  |   |')
-print('_.__/_____|_.__/ \n\n')
-
-SpreadArbRun(verbose=True, libPath='./Pipeline/SpreadArb/main/lib/').run()
