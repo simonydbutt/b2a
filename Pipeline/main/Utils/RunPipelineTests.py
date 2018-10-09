9e7dfb06d977ca303d277e196e63fa898f579ba5
simonydbutt <simonydbutt@gmail.com>
2018-10-09 01:02:43 +0100

    lil update

--- Pipeline/main/Utils/RunPipelineTests.py
+++ Pipeline/main/Utils/RunPipelineTests.py
@@ -1,11 +1,13 @@
 import py
 import Settings
 import os
+import logging
 
 
 class RunPipelineTests:
 
     def __init__(self):
+        logging.info('Starting Pipeline pytests')
         currentDir = os.getcwd()
         os.chdir('%s/Pipeline' % Settings.BASE_PATH)
         py.test.cmdline.main()
