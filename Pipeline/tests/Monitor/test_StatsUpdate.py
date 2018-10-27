65f56cbd635da9f5e241f1e76bbf6968ae139dcb
simonydbutt <simonydbutt@gmail.com>
2018-10-27 15:46:54 +0100

    statsupdate bug and fixed tests

--- Pipeline/tests/Monitor/test_StatsUpdate.py
+++ Pipeline/tests/Monitor/test_StatsUpdate.py
@@ -1,13 +1,52 @@
 from Pipeline.main.Monitor.StatsUpdate import StatsUpdate
 from Pipeline.tests.CreateCleanDir import CreateCleanDir
+from pandas.util.testing import assert_frame_equal
 from pymongo import MongoClient
+import pandas as pd
 import Settings
+import logging
 import yaml
 
 
 client = MongoClient("localhost", 27017)
 dbPath = "Pipeline/resources"
 CCD = CreateCleanDir(["%s/testStrat1" % dbPath, "%s/testStrat2" % dbPath])
+dummy_positions = [
+    {
+        "assetName": "ETHBTC",
+        "openPrice": 0.0000158,
+        "currentPrice": 10,
+        "periods": 0,
+        "hitPrice": 11,
+        "positionSize": 0.4995,
+        "paperSize": 0.4995,
+        "TSOpen": 1534711395,
+        "sellPrice": 9,
+        "exchange": "Binance",
+    },
+    {
+        "assetName": "LTCBTC",
+        "openPrice": 0.0000158,
+        "currentPrice": 0.0000158,
+        "periods": 6,
+        "sellPrice": 9,
+        "positionSize": 0.4995,
+        "paperSize": 0.4995,
+        "TSOpen": 1534711395,
+        "hitPrice": 11,
+        "exchange": "Binance",
+    },
+]
+currentStatsDF = pd.DataFrame(
+    [
+        ["ETHBTC", 10, 0.0000158, 0, 63291139.2405],
+        ["LTCBTC", 0.0000158, 0.0000158, 0.12, 100],
+    ],
+    columns=["assetName", "currentPrice", "openPrice", "daysOpen", "%"],
+)
+currentEmptyStats = pd.DataFrame(
+    [], columns=["assetName", "currentPrice", "openPrice", "daysOpen", "%"]
+)
 
 
 def before():
@@ -16,7 +55,7 @@ def before():
     CCD.create()
     for strat in ("testStrat1", "testStrat2"):
         colDB = client[strat]["currentPositions"]
-        colDB.insert_one({"assetName": "LTCBTC"})
+        colDB.insert_many(dummy_positions)
         transDB = client[strat]["transactionLogs"]
         transDB.insert_many([{"a": 1}, {"b": 2}, {"c": 2}])
         capitalDict = {
@@ -41,10 +80,10 @@ def test_indivStats():
     SU = StatsUpdate()
     stats = SU.indivStats("testStrat1")
     assert stats["initialCapital"] == 10
-    assert stats["numberOpen"] == 1
+    assert stats["numberOpen"] == 2
     assert stats["numberTransactions"] == 3
-    assert stats["openList"] == ["LTCBTC"]
-    assert stats["paperAvgPnL"] == 0.025
+    assert stats["openList"] == ["ETHBTC", "LTCBTC"]
+    assert stats["paperAvgPnL"] == 0.02
     after()
 
 
@@ -54,7 +93,7 @@ def test_compStats():
     statDict = SU.compStats(isTest=["testStrat1", "testStrat2"])
     assert len(list(statDict)) == 3
     totDict = statDict["total"]
-    assert totDict["numberOpen"] == 2
+    assert totDict["numberOpen"] == 4
     assert totDict["initialCapital"] == 20
     assert totDict["liquidCurrent"] == 18
     assert totDict["paperCurrent"] == 22
@@ -62,6 +101,22 @@ def test_compStats():
     after()
 
 
+def test_getCurrentStats():
+    before()
+    df = StatsUpdate().getCurrentStats(stratName="testStrat1")
+    assert_frame_equal(df, currentStatsDF)
+    client.testStrat1.drop_collection("currentPositions")
+    df = StatsUpdate().getCurrentStats(stratName="testStrat1")
+    assert_frame_equal(df, currentEmptyStats)
+    after()
+
+
 if __name__ == "__main__":
-    test_indivStats()
-    test_compStats()
+    logging.basicConfig(level=logging.DEBUG)
+    try:
+        test_indivStats()
+        test_compStats()
+        test_getCurrentStats()
+    except AssertionError:
+        after()
+        raise AssertionError
